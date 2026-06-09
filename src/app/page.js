e96deda5a057eed8b7c2dbbaa79caa97278e286e
Gabrielly Sales <gabriellysales13@gmail.com>
2026-06-09 14:18:44 -0300

    Chamando os pokemons pela API

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,34 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+export async function getPokemons() {
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+  
+  return data.results
+}
+
+export default async function Home() {
+
+  const pokemons = await getPokemons()
+
   return (
-    <>
+    <div>
       <h1>PokeNext</h1>
-    </>
+
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+
+    </div>
   );
 }
